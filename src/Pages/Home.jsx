3b43e70cb75d1383d2d8969abd589e48acaf1da6
LeoFuna <leo.funa@gmail.com>
2021-09-22 18:03:54 -0300

    ADD componente about

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import Header from "../Components/Header";
 import Apresentation from "../Components/Apresentation";
+import About from "../Components/About";
 
 function Home() {
   return(
     <div>
       <Header />
+      <div style={{ height: '10vh' }} /> { /* Div colocada para compensar o tamanho do header por ele ser fixed */ }
       <Apresentation />
+      <About />
     </div>
   );
 }
