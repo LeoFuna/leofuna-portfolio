7529fecf251b8a34a48d6e97687eb88b439e4cfe
LeoFuna <leo.funa@gmail.com>
2021-09-22 13:44:49 -0300

    interaçao aparecer lista no click realizado

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,12 @@ import React from "react"
 import "../Styles/Header.css";
 
 function Header() {
+
+  function handleOpenSideBar() {
+    const navMenuElement = document.getElementsByClassName("nav-menu")
+    navMenuElement[0].classList.toggle("visible")
+  }
+
   return(
     <header>
       <div className="logo-container">
@@ -9,22 +15,19 @@ function Header() {
         <p>Leonardo Funabashi</p>
       </div>
       <nav className="nav-menu-container">
-        <div className="nav-list-container">
-          <ul className="nav-menu">
-            <li className="nav-item">Home</li>
-            <li className="nav-item">Projects</li>
-            <li className="nav-item">Contact</li>
-          </ul>
-          <div className="nav-list-menu-logo">
-            <input type="checkbox" id="checkbox-menu" />
-            <label className="list-lines" htmlFor="checkbox-menu">
-              <span />
-              <span />
-              <span />
-            </label>
-          </div>
+        <div className="nav-list-menu-logo">
+          <input type="checkbox" onClick={ handleOpenSideBar } id="checkbox-menu" />
+          <label className="list-lines" htmlFor="checkbox-menu">
+            <span />
+            <span />
+            <span />
+          </label>
         </div>
-        <div className="nav-list-menu-logo-close">Sair</div>
+        <ul className="nav-menu">
+          <li className="nav-item">Home</li>
+          <li className="nav-item">Projects</li>
+          <li className="nav-item">Contact</li>
+        </ul>
       </nav>
     </header>
   );
