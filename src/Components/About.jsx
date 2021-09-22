ba12e02eb5dbb01954ca3ad7a21779af3dfbc2bd
LeoFuna <leo.funa@gmail.com>
2021-09-22 18:09:43 -0300

    ADJUST coloca em 2 paragrafos o about

---
+++ src/Components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../Styles/About.css";
+
+function About() {
+  return (
+    <div className="about-container">
+      <h1>About</h1>
+      <p>Graduated in production engineering and passionate about technology, finance, innovation, musical instruments and dogs.</p>
+        <p>I love to think about how technology can be, as it already is, transformative for the world.</p>
+    </div>
+  );
+}
+
+export default About;
