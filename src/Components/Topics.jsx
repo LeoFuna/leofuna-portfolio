40735ba106c65a8fe0fbe61bdf42477e6e69f672
LeoFuna <leo.funa@gmail.com>
2021-09-23 16:21:00 -0300

    Primeiros passados dos topicos

--- src/Components/Topics.jsx
+++ src/Components/Topics.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import "../Styles/Topics.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartLine, faGuitar, faLaptopCode, faLightbulb, faSeedling } from '@fortawesome/free-solid-svg-icons';
+import { faCss3, faGithubAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
 
 function Topics() {
   return (
@@ -8,26 +11,50 @@ function Topics() {
         <h1>Interest</h1>
         <ul className="topic-list">
           <li className="topic">
-            <span>lo</span>
+            <FontAwesomeIcon icon={faLaptopCode} />
             <p>Technology</p>
           </li>
-          <li className="topic"></li>
-          <li className="topic"></li>
-          <li className="topic"></li>
-          <li className="topic"></li>
+          <li className="topic">
+            <FontAwesomeIcon icon={ faLightbulb } />
+            <p>Inovation</p>
+          </li>
+          <li className="topic">
+            <FontAwesomeIcon icon={ faChartLine } />
+            <p>Finance</p>
+          </li>
+          <li className="topic">
+            <FontAwesomeIcon icon={ faGuitar } />
+            <p>Music</p>
+          </li>
+          <li className="topic">
+            <FontAwesomeIcon icon={ faSeedling } />
+            <p>Minimalism</p>
+          </li>
         </ul>
       </div>
       <div className="hard-skills-container">
         <h1>Hard Skills</h1>
         <ul className="topic-list">
           <li className="topic">
-            <span>logo</span>
-            <p>Technology</p>
+            <FontAwesomeIcon icon={ faGithubAlt } />
+            <p>Github</p>
           </li>
-          <li className="topic"></li>
-          <li className="topic"></li>
-          <li className="topic"></li>
-          <li className="topic"></li>
+          <li className="topic">
+            <FontAwesomeIcon icon={ faHtml5 } />
+            <p>Html</p>
+          </li>
+          <li className="topic">
+            <FontAwesomeIcon icon={ faCss3 } />
+            <p>Css</p>
+          </li>
+          <li className="topic">
+            <FontAwesomeIcon icon={ faJsSquare } />
+            <p>Javascript</p>
+          </li>
+          <li className="topic">
+            <FontAwesomeIcon icon={ faReact } />
+            <p>React</p>
+          </li>
         </ul>
       </div>
     </div>
