7f78284d0304a309530bff5792ac832ce25a9b10
LeoFuna <leo.funa@gmail.com>
2021-12-02 09:58:30 -0300

    feat: link dos contatos realizado e pagina finalizada

--- src/Components/ContactGreetings.jsx
+++ src/Components/ContactGreetings.jsx
@@ -5,7 +5,7 @@ function ContactGreetings() {
   return (
     <div className='greetings-container'>
       <h1>Contate o <span className='contact-span'>Funa</span></h1>
-      <h3>Onde iremos nos conectar?</h3>
+      <h3>Por onde iremos nos conectar?</h3>
     </div>
   );
 }
--- src/Components/ContactIcons.jsx
+++ src/Components/ContactIcons.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithubAlt, faLinkedinIn, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faGithubAlt, faLinkedinIn, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import React from 'react';
 import '../Styles/ContactIcons.css';
 
@@ -7,28 +7,28 @@ function ContactIcons() {
   return (
     <div className='icons-container'>
       <div>
-        <div>
-          <FontAwesomeIcon icon={faGithubAlt} />
-        </div>
-        <h3>Github</h3>
+        <a href="https://github.com/LeoFuna" target="_blank" rel="noreferrer">
+          <div>
+            <FontAwesomeIcon icon={faGithubAlt} />
+          </div>
+          <h3>Github</h3>
+        </a>
       </div>
       <div>
-        <div>
-          <FontAwesomeIcon icon={faLinkedinIn} />
-        </div>
-        <h3>Linkedin</h3>
+        <a href="https://www.linkedin.com/in/leonardofunabashi/" target="_blank" rel="noreferrer">
+          <div>
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </div>
+          <h3>Linkedin</h3>
+        </a>
       </div>
       <div>
-        <div>
-          <FontAwesomeIcon icon={faInstagram} />
-        </div>
-        <h3>Instagram</h3>
-      </div>
-      <div>
-        <div>
-          <FontAwesomeIcon icon={faWhatsapp} />
-        </div>
-        <h3>Whatsapp</h3>
+        <a href="https://wa.me/55998295835" target="_blank" rel="noreferrer">
+          <div>
+            <FontAwesomeIcon icon={faWhatsapp} />
+          </div>
+          <h3>Whatsapp</h3>
+        </a>
       </div>
     </div>
   );
