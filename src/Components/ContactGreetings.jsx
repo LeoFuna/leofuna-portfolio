f3ef8129c334b8c133765d49ceeb9662fd7cf329
LeoFuna <leo.funa@gmail.com>
2021-12-02 10:08:34 -0300

    test: teste do componente ContactGreetings

--- src/Components/ContactGreetings.jsx
+++ src/Components/ContactGreetings.jsx
@@ -3,7 +3,7 @@ import '../Styles/ContactGreetings.css'
 
 function ContactGreetings() {
   return (
-    <div className='greetings-container'>
+    <div data-testid='greetings-container' className='greetings-container'>
       <h1>Contate o <span className='contact-span'>Funa</span></h1>
       <h3>Por onde iremos nos conectar?</h3>
     </div>
